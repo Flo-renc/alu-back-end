43dfec453e0ac3093b01dcd40e079f6701b70acb
Flo-renc <f.kabeya@alustudent.com>
2024-06-28 18:48:44 -0700

    pep8 validation checks

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """
-Python Scrip that exports data in JSON format.
+Python script that exports data in JSON format.
 """
 import json
 import requests
 if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     users = requests.get(url + "users").json()
-    # Export to JSON
     with open("todo_all_employees.json", "w") as jsonfile:
         json.dump({
             user.get("id"): [{
@@ -18,4 +17,4 @@ if __name__ == "__main__":
                 url + "todos", params={"userId": user.get("id")}
             ).json()]
             for user in users
-        }, jsonfile))
+        }, jsonfile)
